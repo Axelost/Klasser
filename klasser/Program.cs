9acddf0e4ba63179967c652e1afa9379f181006e
Axelost <axel.osterlund1@gmail.com>
2017-01-13 14:57:48 +0100

    Klasser

--- klasser/Program.cs
+++ klasser/Program.cs
@@ -10,6 +10,24 @@ namespace klasser
     {
         static void Main(string[] args)
         {
+            BilInfo BInfo = new BilInfo();
+
+            //Console.Write("Ange märke på bilen: ");
+            //BInfo.Märke = Console.ReadLine();
+            //Console.Write("Ange Årsmodell på bilen: ");
+            //BInfo.Modell = Console.ReadLine();
+            //Console.Write("Ange antal däck på bilen: ");
+            //BInfo.Däck = Console.ReadLine();
+            //Console.Write("Ange vikt på bilen: ");
+            //BInfo.Vikt = Console.ReadLine();
+            //Console.Write("Ange färg på bilen: ");
+            //BInfo.Färg = Console.ReadLine();
+
+            //Console.WriteLine(BInfo.Märke+"\n"+BInfo.Modell+"\n"+BInfo.Däck+"\n"+BInfo.Vikt+"\n"+BInfo.Färg);
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ klasser/bilinfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser
+{
+    class BilInfo
+    {
+        public string Märke { get; set; }
+        public string Modell { get; set; }
+        public string Däck { get; set; }
+        public string Vikt { get; set; }
+        public string Färg { get; set; }
+    }
+}
